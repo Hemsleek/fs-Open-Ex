04bf1e3a55acaa6922ce4d95401bc380affa93e3
hemsleek <asiyanbimubashir@gmail.com>
2021-01-18 21:45:27 +0100

    update: anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,25 +3,33 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points , setPoints] = useState( new Array(anecdotes.length).fill(0))
 
   //creates a random number between 0 and length of anecdotes array inclusive
   const randNumber = () => Math.floor(Math.random() * anecdotes.length)
 
-  //creates an array
-  const points = [].fill(0,0,anecdotes.length)
-  console.log(points);
-
   const handlePoints = () => {
     const counts = [...points]
     counts[selected] += 1
+    setPoints(counts)
   }
-
+  const highestVotes = Math.max(...points)
+  const highestQuote = points.indexOf(highestVotes)
   return (
     <div className="App">
-      <p>{props.anecdotes[selected]}</p>
-      <button onClick={handlePoints} style={{marginRight:10}}>Vote</button>
-      <button onClick={() => setSelected(randNumber())}>Next Anecdotes</button>
-      <p>has { points[selected] }</p>
+      <div>
+        <h2>Anecdote of the day</h2>
+        <p>{props.anecdotes[selected]}</p>
+        <button onClick={handlePoints} style={{marginRight:10}}>Vote</button>
+        <button onClick={() => setSelected(randNumber())}>Next Anecdotes</button>
+        <p>has { points[selected] } Votes</p>
+      </div>
+      { highestVotes!==0 && 
+        <div>
+          <h2>Anecdote with most votes</h2>
+          <p>{props.anecdotes[highestQuote]}</p> 
+        </div>
+      }
     </div>
   )
 }
